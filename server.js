da15d0fce17c2f5e34bc9f3d456876b071296a3e
NertNiels <nertniels@gmail.com>
2022-07-16 13:08:18 +0200

    for linode

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const public = __dirname + '/docs/';
 
-app.use(express.static('docs'));
+app.use(express.static(public));
 
 app.get('/', (req, res) => {
 	res.sendFile(public + 'home.html')
